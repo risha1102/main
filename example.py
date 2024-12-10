acbec1a2463e100817cc2a60a4a135574219bea2
risha1102 <risha.kothari@accenture.com>
2024-12-10 10:13:59 +0530

    Add files via upload

---
+++ example.py
@@ -0,0 +1,17 @@
+
+
+def main():
+    print("Welcome to MyPythonRepo!")
+    greet("Alice")
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+def farewell():
+    print("Good Bye")
+
+
+if __name__ == "__main__":
+    main()
